1246ad6c8e93bb34467ae92e2c1fca7fe00501c6
Chris Young and David Remington <david.ian.remington@gmail.com>
2016-03-14 15:21:46 -0700

    switch simon.js to be an object with prototypes

--- js/browser-interface.js
+++ js/browser-interface.js
@@ -3,7 +3,15 @@ var simon = require('./../js/simon.js');
 $(function(){
   $('#start').click(function() {
     var color = simon.simon();
-    var colorBox = document.getElementById("color-box");
-    colorBox.style.backgroundColor = color;
+    color.forEach{
+      var colorBox = document.getElementById("color-box");
+      colorBox.style.backgroundColor = color[i];
+    }
   });
 });
+
+//Game is initiated
+//Computer sequence at [0] is added
+//Player Guesses a color
+//guessCheck() is run.
+//If gameOn = true; repeat
--- js/simon.js
+++ js/simon.js
@@ -1,14 +1,31 @@
 module.exports.simon = simon;
 
-function simon() {
-  var color = Math.floor(Math.random() * 4);
-  if(color === 0) {
-    return "red";
-  } else if (color === 1) {
-    return "green";
-  } else if (color === 2) {
-    return "blue";
-  } else {
-    return "yellow";
-  }
+function Simon() {
+  this.computerSequence = [];
+  this.userSequence = [];
+  this.counter=0;
+  this.gameOn=true;
 };
+
+
+Simon.prototype.computerTurn = function() {
+  var color = Math.floor(Math.random() * 4);
+
+  if(color === 0) {
+    computerSequence.push("red");
+  } else if (color === 1) {
+    computerSequence.push("green");
+  } else if (color === 2) {
+    computerSequence.push("blue");
+  } else {
+    computerSequence.push("yellow");
+  }
+}
+
+Simon.prototype.checkGuess = function() {
+    this.counter++;
+    if (computerSequence[counter-1] != computerSequence[counter-1]){
+      this.gameOn=false;
+    }
+  }
+}
